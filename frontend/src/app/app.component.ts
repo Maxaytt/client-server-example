0defd65101685823a07dbe49494820a6e5f63e64
maxaytt <Maxaytt@gmail.com>
2024-02-09 17:58:28 +0100

    feat: get data from api

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,15 +1,33 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormsModule],
-  template: `<label>Имя маля:</label>
-            <input [(ngModel)]="name" placeholder="name">
-            <h1>Добро пожаловать {{name}}!</h1>`
+  imports: [FormsModule, CommonModule, HttpClientModule],
+  template: `
+    <label>Hello suka</label>
+    <button (click)="getData()">Кнопочка</button>
+    <div *ngIf="data">
+      <pre>{{ data | json}}</pre>
+    </div>
+    `
 })
 export class AppComponent {
+  data:any;
   title = 'frontend';
   name='';
+  constructor(private httpClient: HttpClient) {}
+
+  getData() {
+    this.httpClient.get('http://localhost:5283/WeatherForecast').subscribe({
+      next: res => {
+        console.log('data received', res);
+        this.data = res;
+      },
+      error: err => console.error('error when retrieving data', err)
+    })
+  }
 }
